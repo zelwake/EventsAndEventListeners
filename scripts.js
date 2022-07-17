3243dded1aebacb99b33257b4294ae7c86274844
zelwake <michalhladik2@seznam.cz>
2022-07-17 16:38:32 +0200

    v2

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
-$("button").click(() => {
+$("#firstButton").click(() => {
     console.log("Yeah, you clicked me")
+})
+
+$("#secondButton").click(() => {
+    $("#firstButton").text("Clicked");
 })
